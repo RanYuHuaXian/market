dc9d43af544b141eaf56f8716a68ebd0ecb05d33
Lenny <2029194345@qq.com>
2023-04-17 16:53:55 +0800

    初始化项目

---
+++ market/src/main/java/org/example/market/SpringApp.java
@@ -0,0 +1,11 @@
+package org.example.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//用于启动SpringBoot项目的类
+@SpringBootApplication
+public class SpringApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp.class);
+    }
+}
